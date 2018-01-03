61ee0456ae04127f25c9c31e976802e8c8fb8c2d
Sarah Kathryn Stein <skstein@stanford.edu>
2018-01-03 13:29:16 -0800

    Update 1/3/2018

--- scrape_allrecipes.py
+++ scrape_allrecipes.py
@@ -22,7 +22,7 @@ chromedriver = '/usr/local/bin/chromedriver'
 os.environ['webdriver.chrome.driver'] = chromedriver
 driver = webdriver.Chrome(chromedriver)
 
-pages = ['https://allrecipes.com/recipes/17562/dinner/?page=' + str(p) for p in range(36,37)]
+pages = ['https://allrecipes.com/recipes/17562/dinner/?page=' + str(p) for p in range(41,42)]
 
 for j in pages:
 
